03ed6b5a8573b5bd3fa10df9a9a694d4cf58bedc
Владислав Ковалёв <vladislav.kovalev@autopolygon.base>
2022-12-08 17:12:24 +0300

    make migrations

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Favorites, Ingredient, Ingredients_quantity, Recipe,
+                     Order_cart, Tag)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'author', 'quantity_in_favorites')
+    readonly_fields = ('quantity_in_favorites',)
+    list_filter = ('author', 'name', 'tags',)
+
+    #@display(description='Добавлено в избранное')
+    def quantity_in_favorites(self, obj):
+        return obj.favorites.count()
+#    quantity_in_favorites.description='Добавлено в избранное'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('title', 'unit',)
+    list_filter = ('title',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color_hex', 'slug',)
+
+
+@admin.register(Order_cart)
+class OrderCartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe',)
+
+
+@admin.register(Favorites)
+class FavouriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe',)
+
+
+@admin.register(Ingredients_quantity)
+class IngredientInRecipe(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'quantity',)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Follow, User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name', 'email',)
+    list_filter = ('email', 'first_name')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author',)
