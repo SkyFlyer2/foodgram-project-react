02e06142245065485577a17b6ed55e84bb5804c5
Владислав Ковалёв <vladislav.kovalev@autopolygon.base>
2022-12-13 10:29:54 +0300

    config

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_URLS_REGEX = r'^/api/.*$' 
+CORS_URLS_REGEX = r'^/api/.*$', '^/signin/.*$',
 
 #CORS_ALLOWED_ORIGINS = [
 #    'http://localhost:3000',
