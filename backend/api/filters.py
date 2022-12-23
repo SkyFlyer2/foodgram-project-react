3db8e8d34169d2c9fd2388b7b923bd5c9532fa48
Владислав Ковалёв <vladislav.kovalev@autopolygon.base>
2022-12-23 17:15:11 +0300

    edit filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django_filters import rest_framework as filters
 from recipes.models import Recipe, Ingredient
 
 
@@ -15,19 +16,19 @@ class IngredientsSearchFilter(django_filters.FilterSet):
         fields = ("name",)
 
 
-class RecipeAndTagsFilter(django_filters.FilterSet):
-    is_in_shopping_cart = django_filters.BooleanFilter(
+class RecipeAndTagsFilter(filters.FilterSet):
+    is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart'
     )
-    is_favorited = django_filters.BooleanFilter(method='filter_is_favorited')
-    tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug')
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
 
     class Meta:
         model = Recipe
         fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
-        print('value=',value)
+        print('value=', value)
         if self.request.user.is_authenticated and value:
             #return queryset.filter(in_favorite__user=self.request.user)
             return queryset.filter(favorites__user=self.request.user)
