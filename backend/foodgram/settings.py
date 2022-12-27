de17352c1634cbf529bbb4a83a258444a04a88e6
Владислав Ковалёв <vladislav.kovalev@autopolygon.base>
2022-12-27 12:52:17 +0300

    settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = str(os.getenv('SECRET_KEY', default='*'))
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'users.User'
